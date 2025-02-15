ef1ea36a66520e85d67843a66cbf5d2a085488ef
SamirFezani <samirfezani3@gmail.com>
2025-02-16 00:50:49 +0100

    Correction : Ajout de REPORTS_PATH dans config.py

--- scripts/config.py
+++ scripts/config.py
@@ -7,6 +7,8 @@ load_dotenv()
 # Définition des chemins
 RAW_DATA_PATH = os.getenv("RAW_DATA_PATH", "data/raw")
 PROCESSED_DATA_PATH = os.getenv("PROCESSED_DATA_PATH", "data/processed")
+PROCESSED_DATA_PATH = os.path.join(os.getcwd(), "data", "processed")
+REPORTS_PATH = os.path.join(os.getcwd(), "reports")
 
 # Dataset Kaggle
 KAGGLE_DATASET = "retailrocket/ecommerce-dataset"
